a1ca347b3c696dfabf36cbb90cd2597f040842a8
Deen-q <124711768+Deen-q@users.noreply.github.com>
2023-11-30 17:26:17 +0000

    successful log messages with EnqueueMessage but fail to enqueue messages using Azure Storage Emulator and Explorer

--- Function1.cs
+++ Function1.cs
@@ -8,9 +8,19 @@ namespace FirstCSharp_App
     public static class QueueFunction
     {
         [FunctionName("QueueFunction")]
-        public static void Run([QueueTrigger("myQueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        public static void Run(
+            [QueueTrigger("myQueue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed myQueueItem: {myQueueItem}");
+        }
+        [FunctionName("EnqueueMessage")]
+        public static void EnqueueMessage(
+            [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+            [Queue("myQueue"), StorageAccount("AzureWebJobsStorage")] out string message,
+            ILogger log)
+        {
+            message = "Hello from EnqueueMessage!";
+            log.LogInformation($"C# Timer trigger function enqueued a message at: {DateTime.Now}");
         }
     }
 }
